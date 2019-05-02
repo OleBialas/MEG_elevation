65a8b420bfa0f9fbe100ed3647eaf8a841073813
Ole <bialas@cbs.mpg.de>
2019-05-02 15:40:26 +0200

    machine learning

--- py/eeg_pipeline.py
+++ py/eeg_pipeline.py
@@ -1,30 +1,11 @@
 import os
 import sys
 sys.path.append(os.environ["PYDIR"])
-from mne import find_events, concatenate_raws, pick_types
-from mne.epochs import Epochs
-from mne.io import read_raw_fif
-from matplotlib import pyplot as plt
-import numpy as np
-from mne.channels import read_montage
+from mne import read_epochs
+from mne.preprocessing import read_ica
 
+os.environ["SUBJECT"] = "eegl03"
+epochs = read_epochs(os.path.join(os.environ["EXPDIR"],os.environ["SUBJECT"],os.environ["SUBJECT"]+"_Augenmitte-epo.fif"))
+ica = read_ica(os.path.join(os.environ["EXPDIR"],os.environ["SUBJECT"],os.environ["SUBJECT"]+"-ica.fif"))
 
-subjects=["eegl01","eegl02","eegl03","eegl04","eegl05","eegl06","eegl07","eegl08"]
-condition="Augenmitte"
-n_blocks=4
-montage = read_montage("easycap-M10")
-# load data for all subjects in dictionary:
-raws=[]
-for subject in subjects:
-	os.environ["SUBJECT"] = subject
-	for i in range(1,n_blocks+1):
-		raws.append(read_raw_fif(os.path.join(os.environ["EXPDIR"],os.environ["SUBJECT"],os.environ["SUBJECT"]+"_"+str(i)+condition+"_filt.raw")))
-raw = concatenate_raws(raws)
-raw.info["bads"] =["1","2","64"]
-events = find_events(raw)
-picks = pick_types(raw.info)
-tmin = -0.1
-tmax = 1
-baseline = (-0.1,0)
-reject_thresh = dict(eeg=0.0002)
-epochs = Epochs(raw, events, tmin=tmin, tmax=tmax, baseline=baseline, reject=reject_thresh, preload=True)
+#reject components 0 (blinks), 5(eye-movement), 9,11,14 and 17 (high frequency noise)
