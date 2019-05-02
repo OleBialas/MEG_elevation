c66293a449f6d2f6c7d9c2fdea1b3394905b4546
Ole <bialas@cbs.mpg.de>
2019-05-02 15:53:26 +0200

    machine learning

---
+++ py/machine_learning_eeg.py
@@ -0,0 +1,99 @@
+import os
+from mne import read_epochs
+from mne.preprocessing import read_ica
+from matplotlib import pyplot
+from numpy import vstack, array, mean, std
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, scale
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import classification_report
+
+
+
+def classify_epochs(epochs, test_size=0.5, decision_function_shape="ovr"):
+
+    epoch_data = [epoch.T for epoch in epochs_ica._data]
+    targets=epochs_ica.events[:,2]
+    data = create_dataset(epoch_data, targets, n_vars=epoch_data[0].shape[1], n_steps=epoch_data[0].shape[0])
+    X, y = data[:, :-1], data[:, -1] # split into inputs and outputs
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=test_size)
+    clf = svm.SVC(kernel="linear", decision_function_shape=decision_function_shape)
+    clf.fit(X_train, y_train)
+    pred_clf=cfl.predict(X_test)
+    print(classification_report(y_test, pred_clf))
+
+    return pred_clf
+
+
+
+def compare_models(epochs):
+
+    epoch_data = [epoch.T for epoch in epochs_ica._data]
+    targets=epochs_ica.events[:,2]
+    data = create_dataset(epoch_data, targets, n_vars=epoch_data[0].shape[1], n_steps=epoch_data[0].shape[0])
+    X, y = data[:, :-1], data[:, -1] # split into inputs and outputs
+
+    models, names = list(), list() # create a list of models to evaluate
+    models.append(LogisticRegression())
+    names.append('LR') # logistic
+    models.append(KNeighborsClassifier())
+    names.append('KNN') # knn
+    models.append(DecisionTreeClassifier())
+    names.append('CART') # cart
+    models.append(SVC())
+    names.append('SVM') # svm
+    models.append(RandomForestClassifier())
+    names.append('RF') # random forest
+    models.append(GradientBoostingClassifier())
+    names.append('GBM') # gbm
+
+    # estimtate pefromance accuracy for each algorithm, using 5-fold cross-validation
+    all_scores = list()
+    for i in range(len(models)):
+    	# create a pipeline for the model
+    	s = StandardScaler()
+    	p = Pipeline(steps=[('s',s), ('m',models[i])])
+    	scores = cross_val_score(p, X, y, scoring='accuracy', cv=5, n_jobs=-1)
+    	all_scores.append(scores)
+    	# summarize
+    	m, s = mean(scores)*100, std(scores)*100
+    	print('%s %.3f%% +/-%.3f' % (names[i], m, s))
+    pyplot.boxplot(all_scores, labels=names)
+    pyplot.show()
+
+
+def create_dataset(sequences, targets, n_vars=4, n_steps=19):
+
+	transformed = list() 	# create the transformed dataset
+	# process each trace in turn
+	for i in range(len(sequences)):
+		seq = sequences[i]
+		vector = list()
+
+		for row in range(1, n_steps+1):
+			for col in range(n_vars):
+				vector.append(seq[-row, col])
+		# add output
+		vector.append(targets[i])
+		# store
+		transformed.append(vector)
+	# prepare array
+	transformed = array(transformed)
+	transformed = transformed.astype('float32')
+	return transformed
+
+if __name__ =="__main__":
+    os.environ["SUBJECT"] = "eegl03"
+    epochs = read_epochs(os.path.join(os.environ["EXPDIR"],os.environ["SUBJECT"],os.environ["SUBJECT"]+"_Augenmitte-epo.fif"))
+    ica = read_ica(os.path.join(os.environ["EXPDIR"],os.environ["SUBJECT"],os.environ["SUBJECT"]+"-ica.fif"))
+    epochs_ica = ica.apply(epochs) #reject components 0 (blinks) & 5(eye-movement)
+    epochs_ica.apply_baseline(baseline=(0.5,0.6)) # use 100ms before stimulus onset as baseline
+    epochs_ica.crop(tmin=0.5,tmax=1.0)
+    epochs_ica = epochs_ica["1","2","3"]
+    pred_clf=classify_epochs(epochs_ica)
